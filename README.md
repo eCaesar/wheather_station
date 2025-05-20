ada944b8a31c492d49072b656ad0bf7051930e4c
eCaesar <georgy.gaykov@gmail.com>
2025-05-20 12:53:44 +0300

    Fix CSV parsing and save wind rose

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # wheather_station
 CSV logs from commercial whether station and  wind analysis code in python
 
-The python file plots wind roses for wind and gust values in the file. 
-It also calculates max values of wind and gust and mean wind speed.
+The python file plots wind roses for wind and gust values in the file and saves
+the resulting figure to `wind_rose.png`.
+It also calculates max values of wind and gust and the mean wind speed.
 
 The file does not consider date and time column from the CSV log (which is actually important).
--- wind_analyser.py
+++ wind_analyser.py
@@ -6,6 +6,7 @@ import csv
 import numpy as np
 import math
 from matplotlib.collections import PatchCollection
+from matplotlib.patches import Wedge
 import matplotlib.pyplot as plt
 
 
@@ -14,7 +15,7 @@ maxwind10 = maxwind*10
 unification_factor = 2 # the precion of wind speed scale will be (1 / unification_factor) m/s scale.
 #Possible values 1, 2, 5, 10
 
-# There are 16 possible wind directions in the log (column 13): 
+# There are 16 possible wind directions in the log (column 12):
 direction_names = {'N': 0, 'NNE': 1, 'NE': 2,  'ENE': 3,  'E': 4,  'ESE': 5,  'SE': 6,  'SSE': 7,
                    'S': 8, 'SSW': 9, 'SW': 10, 'WSW': 11, 'W': 12, 'WNW': 13, 'NW': 14, 'NNW': 15}
 ##################################################################################################
@@ -24,18 +25,17 @@ def delete_nulls_in_csv():  #original csv file contains null element because of 
     next(fi) #skip header
     data = fi.read()
     fi.close()
-    fo = open('mynew2.csv', 'wb')
+    fo = open('meteo.csv', 'wb')
     fo.write(data.replace(b'\x00', b''))
     fo.close()
 #################################################################################################
      
 def main():
-    delete_nulls_in_csv()
     data = []
-    with open('mynew2.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)    
+    with open('meteo.csv', newline='', encoding='utf-16') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
         for row in reader:
-            data.append(row) 
+            data.append(row)
            
     wind_list = [[0 for x in range(maxwind10)] for y in range(16)]
     gust_list = [[0 for x in range(maxwind10)] for y in range(16)]
@@ -46,16 +46,16 @@ def main():
     max_data_wind_direction = 0
     #print(len(wind_list))
        
-    for j in range (0, len(data)):
-        splitted_line = data[j][0].split() #split each row in csv file into words
+    for j in range(1, len(data)):
+        row = data[j]
         try:
-            #get direction as a number from column 13:
-            direction = int(direction_names[splitted_line[13]]) 
-            #get wind speed from column 11:
-            speed = (float(splitted_line[11]))
-            mean_speed += speed #integrate all speed values
-            #get wind speed from column 12:
-            gust = (float(splitted_line[12]))
+            # get direction as a number from column 12:
+            direction = int(direction_names[row[11]])
+            # get wind speed from column 10:
+            speed = float(row[9])
+            mean_speed += speed  # integrate all speed values
+            # get gust speed from column 11:
+            gust = float(row[10])
             #search for maximal speed:
             if (speed>max_data_wind):
                     max_data_wind = speed
@@ -91,10 +91,10 @@ def main():
                 colors2.append(math.log(gust_list[direction][speed],10))
     
     #Creating a circular frame:
-    patches.append(Wedge((.0, .0), max_data_wind*2, 0, 360, max_data_wind, label = 'fdg'),)
+    patches.append(Wedge((.0, .0), max_data_wind*2, 0, 360, width=max_data_wind, label='fdg'))
     colors.append(max(colors))
     #Creating a circular frame:
-    patches2.append(Wedge((.0, .0), max_data_gust*2, 0, 360, max_data_gust, label = 'fdg'),)
+    patches2.append(Wedge((.0, .0), max_data_gust*2, 0, 360, width=max_data_gust, label='fdg'))
     colors2.append(max(colors2))
     
 #---------------------------------------------------------------------------------------------
@@ -145,9 +145,10 @@ def main():
     plt.text(-max_data_gust+1, -0.5, 'W', fontsize=15, color = 'tab:gray')
     plt.text(max_data_gust-2, -0.5, 'E', fontsize=15, color = 'tab:gray')
     
-    plt.subplots_adjust(right = 1.8, top = 1.05) #set size for plots    
-    
-    plt.show()
+    plt.subplots_adjust(right = 1.8, top = 1.05)  # set size for plots
+
+    plt.savefig('wind_rose.png')
+    plt.close()
   
 #------------------------------------------------------------------------------------------------
 #       Printing some numerical values  
